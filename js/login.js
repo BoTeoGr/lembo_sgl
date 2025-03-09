606578c697f3575f933910184c45d0ad6523267c
stefanny834 <skaylopez2904@gmail.com>
2025-03-08 21:05:24 -0500

    organice los estilos de todos los actualizar

--- js/login.js
+++ js/login.js
@@ -1,5 +1,52 @@
-window.addEventListener('load', function() {
-    document.querySelector('.loader').classList.toggle("loader2");
-});
+const userLogin ={
+    password: '',
+    email: '',
+    
+}
+
+//Seleccionando elementos 
+const password = document.querySelector('#password');
+const email = document.querySelector('#email');
+const form = document.querySelector('.login__form');
+
+//inputs
+form.addEventListener('input', readText)
+email.addEventListener('input', readText);
+password.addEventListener('input', readText);
 
 
+//Evento submit
+form.addEventListener('submit', function(e){
+    e.preventDefault();
+    const{email,password} = userLogin;
+ 
+    if(email.trim() === '' || password.trim() === ''){
+    
+       showAlert('Todos los campos son obligatorio', true)
+        return;
+    }
+     showAlert('Tu datos han sido enviado sastifactoriamente')
+})
+
+function showAlert(message, error = false){
+    const alert = document.createElement('P')
+
+    alert.textContent = message;
+    alert.classList.add('alert');
+
+    if (error){
+        alert.classList.add('error')
+    }else{
+        alert.classList.add('correct')
+    }
+    form.appendChild(alert)
+
+    setTimeout(()=> {
+        alert.remove()
+     },5000)
+}
+
+//Collback o funcion
+function readText(e){
+    userLogin[e.target.id] = e.target.value;
+}
