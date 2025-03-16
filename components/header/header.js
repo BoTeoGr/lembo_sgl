11c772232e85d2f9583ba8921c84f276ec15b195
Mateo Mosquera Grisales <mateogr72@gmail.com>
2025-03-16 11:50:15 -0500

    header

---
+++ components/header/header.js
@@ -0,0 +1,9 @@
+export default function Header() {
+	return `
+        <div class="header__logo-container">
+				<img src="../imgs/logoSena.svg" alt="Logo SENA" class="logo--sena" />
+				<h1 class="header__title">Crear ciclo de cultivo</h1>
+				<img src="../imgs/sgal.jpeg" alt="Logo SGAL" class="logo--sgal" />
+		</div>
+    `;
+}
--- js/main.js
+++ js/main.js
@@ -1,6 +1,7 @@
 import Footer from "../components/footer/footer.js";
 import Navbar from "../components/navbar/navbar.js";
 import MenuNav from "../components/navbar/menu.js";
+import Header from "../components/header/header.js"
 
 const footer = document.querySelector(".footer");
 if (footer) {
@@ -8,6 +9,12 @@ if (footer) {
 }
 
 const nav = document.querySelector(".nav");
-nav.innerHTML = Navbar();
+if (nav) {
+	nav.innerHTML = Navbar();
+	MenuNav();
+}
 
-MenuNav();
+const headerRoot=document.querySelector(".header")
+if (headerRoot) {
+	headerRoot.innerHTML=Header()
+}
